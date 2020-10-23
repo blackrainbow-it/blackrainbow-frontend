edeea714bbee2318afde8b1b2db98bd510242099
raspyweather <raspyweather@gmail.com>
2020-10-23 22:58:40 +0200

    made app gdpr compliant

--- src/app/services/cloud-vision-service.service.ts
+++ src/app/services/cloud-vision-service.service.ts
@@ -10,25 +10,25 @@ export class CloudVisionServiceService {
   constructor(private readonly http: HttpClient) { }
 
   public postImage(imageData: string): void {
-    this.http.post(`https://vision.googleapis.com/v1/images:annotate?key=${environment.api_key}`,
-      {
-        requests: [
-          {
-            features: [
-              {
-                maxResults: 5,
-                type: 'LABEL_DETECTION'
-              },
-              {
-                maxResults: 5,
-                type: 'DOCUMENT_TEXT_DETECTION'
-              }
-            ],
-            image: {
-              content: imageData
-            }
-          }
-        ]
-      }).subscribe(data => console.log(data));
+    /* this.http.post(`https://vision.googleapis.com/v1/images:annotate?key=${environment.api_key}`,
+       {
+         requests: [
+           {
+             features: [
+               {
+                 maxResults: 5,
+                 type: 'LABEL_DETECTION'
+               },
+               {
+                 maxResults: 5,
+                 type: 'DOCUMENT_TEXT_DETECTION'
+               }
+             ],
+             image: {
+               content: imageData
+             }
+           }
+         ]
+       }).subscribe(data => console.log(data));*/
   }
 }
